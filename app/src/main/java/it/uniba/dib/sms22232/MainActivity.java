0521fce0f5558f62921ed53604c97c5cc8becc5b
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-12-12 12:14:36 +0100

    prova

--- app/src/main/java/it/uniba/dib/sms22232/MainActivity.java
+++ app/src/main/java/it/uniba/dib/sms22232/MainActivity.java
@@ -73,7 +73,6 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
